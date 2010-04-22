355147d2ce94ad778481027b6f56755ac7c7a803
CrowdSense <admin@DevBook.local>
2010-04-22 19:41:42 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="django-wurfl",
+           version="0.1",
+           description="xxx",
+           author="XXX",
+           author_email="xxx@xx.com",
+           packages=find_packages(),
+           include_package_data=True,
+)
